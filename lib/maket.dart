539e2496560e7cc0665735fa313f954934c815d4
Fedor Prokudin <70889713+Ohjat@users.noreply.github.com>
2022-09-30 14:04:01 +0300

    ver0.2

--- lib/maket.dart
+++ lib/maket.dart
@@ -6,55 +6,57 @@ class Maket1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-       color: const Color(0xFF039EA2),
-        child: SafeArea(
-          child: Column(
-            children: [
-              Container(
-                margin: const EdgeInsets.only(top: 80),
-                child: Column(children: const [
-                  Text('Medinow', 
-                   style: TextStyle(color: Colors.white, fontSize: 40), ),
-                  Text('Media With Us!',
-                    style: TextStyle(color: Colors.white, fontSize: 10),), 
-                ]
+    return Scaffold(
+      body: Container(
+         color: const Color(0xFF039EA2),
+          child: SafeArea(
+            child: Column(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(top: 80),
+                  child: Column(children: const [
+                    Text('Medinow', 
+                     style: TextStyle(color: Colors.white, fontSize: 40), ),
+                    Text('Media With Us!',
+                      style: TextStyle(color: Colors.white, fontSize: 10),), 
+                  ]
+                  ),
                 ),
-              ),
-              Container(
-                margin: const EdgeInsets.only(top: 50, left: 25, right: 25),
-                child: Column(
-                  children:  [
-                      TextButton(onPressed: (){}, child: const Text('           ing in with Apple           '),style: ButtonStyle(
-                      padding: MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(15)),
-        foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
-        backgroundColor:  MaterialStateProperty.all<Color>(Colors.white),
-        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-          RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(18.0),),))),
-              TextButton(onPressed: (){}, child: const Text('           Continue with Email or Phone           ')   
-                        ,style: ButtonStyle(
-                      padding: MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(15)),
-        foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
-        backgroundColor:  MaterialStateProperty.all<Color>(Colors.blueAccent),
-        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-          RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(18.0),),))),
-            const Text("Continue with Google", style: TextStyle(color: Colors.white, fontSize: 10), )
-                  ],
-
-                  
+                Container(
+                  margin: const EdgeInsets.only(top: 50, left: 25, right: 25),
+                  child: Column(
+                    children:  [
+                        TextButton(onPressed: (){}, child: const Text('           ing in with Apple           '),style: ButtonStyle(
+                        padding: MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(15)),
+          foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
+          backgroundColor:  MaterialStateProperty.all<Color>(Colors.white),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(18.0),),))),
+                TextButton(onPressed: (){}, child: const Text('           Continue with Email or Phone           ')   
+                          ,style: ButtonStyle(
+                        padding: MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(15)),
+          foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
+          backgroundColor:  MaterialStateProperty.all<Color>(Colors.blueAccent),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(18.0),),))),
+              const Text("Continue with Google", style: TextStyle(color: Colors.white, fontSize: 10), )
+                    ],
+    
+                    
+                  ),
                 ),
-              ),
-              const Expanded(child: Align(
-                alignment: Alignment.bottomCenter,
-                 
-                child: Image(image: AssetImage('assets/Kartina.png'))
-
-              ))
-            ],
-          ),
-        ), 
+                const Expanded(child: Align(
+                  alignment: Alignment.bottomCenter,
+                   
+                  child: Image(image: AssetImage('assets/Kartina.png'))
+    
+                ))
+              ],
+            ),
+          ), 
+      ),
     );
 
     
